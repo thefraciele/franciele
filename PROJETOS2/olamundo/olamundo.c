537884e319715e4b1301bc2b1b747d5f9874dc41
Franciele <francieleduraes22@gmail.com>
2026-05-18 02:37:52 -0300

    adicionei pasta projeto
    pasta projeto

---
+++ PROJETOS2/olamundo/olamundo.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+int main() {
+    printf("olá mundo!\n");
+    return 0;
+
+}
+
---
+++ PROJETOS2/program/supertrunfo.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h> 
+int main () {
+
+    char cidade[50];
+    char estado;
+    char codigo[20];
+    int  população, pontoturistico;
+    float area, pib;
+
+    printf("Digite o nome da cidade:\n");
+    scanf("%c", cidade);
+
+    printf("digite o estado:\n");
+    scanf("%c", &estado );
+
+    pritf("Digite a quantidade de pessas da sua cidade:\n");
+    scanf("%d", &população);
+
+    printf("")
+
+}
